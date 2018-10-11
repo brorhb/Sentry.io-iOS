8cf03b1f22341184d13742cc6920be93c8bc5004
brorhb <brorbrurberg@me.com>
2018-10-11 17:32:56 +0200

    Initial Commit

---
+++ Sentry.io/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Sentry.io
+//
+//  Created by Bror Brurberg on 11/10/2018.
+//  Copyright © 2018 Bror Brurberg. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
